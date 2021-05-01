f1b43c9fdb1647bbe72979751aa9d32c115ad06b
Reynold Morel <reynoldmorel@gmail.com>
2021-04-30 21:29:50 -0400

    Updated from Jenkins

---
+++ README.md
@@ -0,0 +1,7 @@
+# Git config
+```
+ssh-keygen -t rsa -b 4096 -C "reynoldmorel@gmail.com"
+cat ~/.ssh/id_rsa.pub
+```
+
+Add the key in your github account
--- git-push.sh
+++ git-push.sh
@@ -3,4 +3,4 @@ git config --global user.email "reynoldmorel@gmail.com"
 git config --global user.name "Reynold Morel"
 git add .
 git commit -m "Updated from Jenkins"
-git push https://reynoldmorel:RYmp11310212@github.com/reynoldmorel/zggcp-core-deploy.git
+git push git@github.com:reynoldmorel/zggcp-core-deploy.git

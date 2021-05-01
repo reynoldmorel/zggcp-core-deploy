fea019f8d3819a15d7c30cb26a6e2862911619e9
Reynold Morel <reynoldmorel@gmail.com>
2021-04-30 23:22:29 -0400

    Updated from Jenkins

--- git-push.sh
+++ git-push.sh
@@ -1,9 +1,16 @@
 #!/bin/bash
 ssh-add id_rsa
-ssh-add -l
 git config --global user.email "reynoldmorel@gmail.com"
 git config --global user.name "Reynold Morel"
 git config --global user.username "reynoldmorel"
+
+git clone git@github.com:reynoldmorel/zggcp-core-deploy.git
+git checkout feature/dev
+
+rm -rf zggcp
+
+sudo cp -r ../zggcp-core/target/mvn-repo/. /
+
 git add .
 git commit -m "Updated from Jenkins"
 git push

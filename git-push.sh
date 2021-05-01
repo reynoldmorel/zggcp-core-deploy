4092497a81198466b37a907c3cf3d4b454df22f9
Reynold Morel <reynoldmorel@gmail.com>
2021-04-30 21:23:24 -0400

    Updated from Jenkins

---
+++ git-push.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+git config --global user.email "reynoldmorel@gmail.com"
+git config --global user.name "Reynold Morel"
+git add .
+git commit -m "Updated from Jenkins"
+git push https://reynoldmorel:RYmp11310212@github.com/reynoldmorel/zggcp-core-deploy.git

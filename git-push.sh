ea015723793f3d23178c800c0ef2ec9ef82acfbd
Reynold Morel <reynoldmorel@gmail.com>
2021-04-30 21:45:40 -0400

    Updated from Jenkins

--- git-push.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-git config --global user.email "reynoldmorel@gmail.com"
-git config --global user.name "Reynold Morel"
-git remote set-url origin ssh://git@github.com:reynoldmorel/zggcp-core-deploy.git
-git checkout feature/dev
-git add .
-git commit -m "Updated from Jenkins"
-git push --set-upstream origin feature/dev

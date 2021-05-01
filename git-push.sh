b889c380e3c78d51a216f71eeb922a5a64716ccc
Reynold Morel <reynoldmorel@gmail.com>
2021-04-30 23:12:16 -0400

    Updated from Jenkins

---
+++ git-push.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+git config --global user.email "reynoldmorel@gmail.com"
+git config --global user.name "Reynold Morel"
+git config --global user.username "reynoldmorel"
+git add .
+git commit -m "Updated from Jenkins"
+git push
